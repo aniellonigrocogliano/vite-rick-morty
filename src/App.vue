eb15c2be8524879114725a33397c337483ab239e
aniellonigrocogliano <aniello.nigro.cogliano@gmail.com>
2024-05-06 17:28:09 +0200

    add: chiamata axios

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,32 @@
-<script setup>
+<script>
+import axios from "axios";
+
+export default {
+  components: {
+   
+  },
+  data() {
+    return {
+      cardsArray: [],
+    };
+  },
+  created() {
+    axios
+      .get("https://rickandmortyapi.com/api/character", {
+  
+      })
+      .then((resp) => {
+        console.log(resp);
+        this.cardsArray = resp.results;
+        console.log(this.cardsArray);
+      });
+  },
+};
 </script>
 
 <template>
-  <
 </template>
 
-<style scoped>
+<style lang="scss">
 
 </style>
